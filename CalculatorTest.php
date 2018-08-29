534df2024c5d565df6493a2c8a1ca36bbdbdb858
alexeypet5 <alexpet5@mail.ru>
2018-08-30 01:04:17 +0300

    Add files via upload

--- CalculatorTest.php
+++ CalculatorTest.php
@@ -14,10 +14,30 @@ class CalculatorTests extends PHPUnit_Framework_TestCase
         $this->calculator = NULL;
     }
  
-    public function testAdd()
+    public function test1()
     {
-        $result = $this->calculator->add(1, 2);
+        $result = $this->calculator->calculate(1, 2);
         $this->assertEquals(3, $result);
     }
- 
+    public function test2()
+    {
+        $result = $this->calculator->calculate(0, 0);
+        $this->assertEquals(0, $result);
+    }
+ public function test3()
+    {
+        $result = $this->calculator->calculate(-5, 5);
+        $this->assertEquals(0, $result);
+    }
+ public function test4()
+    {
+        $result = $this->calculator->calculate(-5, -5);
+        $this->assertEquals(-10, $result);
+    }
+  public function test5()
+    {
+        $result = $this->calculator->calculate("dsfsd","asdad");
+        $this->assertEquals(0, $result);
+    }
+
 }
